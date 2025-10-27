798dc78963b6f6309977929edc8ec834b2126e8d
Sivaji_Singiri <147472854+SivajiSingiri1619@users.noreply.github.com>
2025-10-27 12:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.82%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -6,10 +6,7 @@ public:
          int div = n/7;
          int ans = (div*28) + (((div+rem)*(div+rem+1))/2 - ((div*(div+1))/2));
          div=div-1;;
-         while(div>0){
-             ans += (div*7);
-             div--;
-         }
+        ans += (((div*(div+1))/2) * 7);
          return ans;
     }
 };
