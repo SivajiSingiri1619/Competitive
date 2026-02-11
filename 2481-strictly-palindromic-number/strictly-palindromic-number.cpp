bc2f272aafb87a3a0f84086e8968296829d4f59c
Sivaji_Singiri <147472854+SivajiSingiri1619@users.noreply.github.com>
2026-02-11 21:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (23.36%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
         
-          for(int i=2;i<=n-2;i++){
-              string s1="",s2="";
-              int temp = n;
-              while(temp > 0){
-                  s1 += to_string(temp%i);
-                  s2 = to_string(temp%i) + s2;
-                  temp/=i;
-              }
-              if(s1 != s2) return false;
+        //   for(int i=2;i<=n-2;i++){
+        //       string s1="",s2="";
+        //       int temp = n;
+        //       while(temp > 0){
+        //           s1 += to_string(temp%i);
+        //           s2 = to_string(temp%i) + s2;
+        //           temp/=i;
+        //       }
+        //       if(s1 != s2) return false;
               
-          }
-          return true;
+        //   }
+        //   return true;
+        return false;
     }
 };
