296565ede1928b39f3aa74a71dfffd6eccedb62c
Sivaji_Singiri <147472854+SivajiSingiri1619@users.noreply.github.com>
2026-03-16 15:27:06 +0530

    Time: 275 ms (53.33%) | Memory: 286.4 MB (53.33%) - LeetSync

--- 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -3,7 +3,7 @@ public:
     int minCost(vector<int>& nums1, vector<int>& nums2) {
         
         
-         map<int,int>mp1,mp2;
+         unordered_map<int,int>mp1,mp2;
          for(int i=0;i<nums1.size();i++){
             mp1[nums1[i]]++;
             mp2[nums2[i]]++;
