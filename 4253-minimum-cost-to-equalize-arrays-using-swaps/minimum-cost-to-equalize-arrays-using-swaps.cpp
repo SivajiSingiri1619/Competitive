0c3b5e2fbd2c2ab3ba59cea510ca6a0b65136f32
Sivaji_Singiri <147472854+SivajiSingiri1619@users.noreply.github.com>
2026-03-16 15:25:40 +0530

    Time: 618 ms (13.33%) | Memory: 291.6 MB (46.67%) - LeetSync

---
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minCost(vector<int>& nums1, vector<int>& nums2) {
+        
+        
+         map<int,int>mp1,mp2;
+         for(int i=0;i<nums1.size();i++){
+            mp1[nums1[i]]++;
+            mp2[nums2[i]]++;
+         }
+         int cnt1 = 0,cnt2=  0;
+         for(auto i:mp1){
+            
+             if((mp2[i.first] + i.second) % 2 != 0) return -1;
+
+             if(mp2[i.first] != i.second){
+                 
+                  int curr = mp2[i.first] + i.second;
+                  if((curr/2) < i.second) cnt1 += (i.second - (curr/2));         
+             }
+         }
+         cout<<cnt1;
+          for(auto i:mp2){
+            
+             if((mp1[i.first] + i.second) % 2 != 0) return -1;
+
+             if(mp1[i.first] != i.second){
+                int curr = mp1[i.first] + i.second;
+                if((curr/2) < i.second) cnt2+= (i.second - (curr/2));   
+             }
+         }
+         cout<<" "<<cnt2;
+         return min(cnt1,cnt2);
+    }
+};
