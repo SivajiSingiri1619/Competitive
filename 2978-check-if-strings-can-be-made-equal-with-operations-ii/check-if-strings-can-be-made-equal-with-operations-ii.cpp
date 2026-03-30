61fc5dc530203847b61543096d047525a7f56296
Sivaji_Singiri <147472854+SivajiSingiri1619@users.noreply.github.com>
2026-03-30 09:24:57 +0530

    Time: 61 ms (19.54%) | Memory: 25.1 MB (14.37%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        string even1 = "" , odd1 = "",even2 = "" , odd2 = "";
+        for(int i=0;i<s1.size();i++){
+            if(i%2){
+                odd1 += s1[i];
+                odd2 += s2[i];
+            }
+            else{
+                even1 += s1[i];
+                even2 += s2[i];
+            }
+        }
+        sort(even1.begin(),even1.end());
+        sort(even2.begin(),even2.end());
+        sort(odd1.begin(),odd1.end());
+        sort(odd2.begin(),odd2.end());
+        if(even1 == even2 && odd1 == odd2) return true;
+        return false;
+    }
+};
