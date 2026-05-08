b351128392261986f0c286c040506d9660ed8fe1
Sivaji_Singiri <147472854+SivajiSingiri1619@users.noreply.github.com>
2026-05-08 12:19:13 +0530

    Time: 555 ms (14.89%) | Memory: 296.4 MB (21.79%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        
-        vector<int>res;
-        res.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-             
-              if(res.back() < nums[i]) res.push_back(nums[i]);
-              else{
-                   int idx  = lower_bound(res.begin(),res.end(),nums[i]) - res.begin();
-                   res[idx] = nums[i];
-              }
-        }
-        return res.size();
+    int dp_mem(int i,int prev,vector<int>& nums,vector<vector<int>>& dp){
           
+            if( i == nums.size()) return 0;
+            if(dp[i][prev+1] != -1) return dp[i][prev+1];
+
+            int skip = dp_mem(i+1,prev,nums,dp);
+            int take  = 0;
+            if(prev == -1 || nums[i] > nums[prev]){
+                take = 1 + dp_mem(i+1,i,nums,dp);
+            }
+            return dp[i][prev+1] = max(skip,take);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return dp_mem(0,-1,nums,dp);
     }
 };
